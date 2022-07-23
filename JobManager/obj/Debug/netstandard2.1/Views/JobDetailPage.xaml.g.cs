4f428a63b72f5462555b6196d8cdd506b6e9d35f
Vinith1697 <97vinith97@gmail.com>
2022-07-22 20:24:59 -0400

    Second commit: Elements like label, text, image and entry was added automation properties

--- JobManager/obj/Debug/netstandard2.1/Views/JobDetailPage.xaml.g.cs
+++ JobManager/obj/Debug/netstandard2.1/Views/JobDetailPage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace JobManager.Views {
     public partial class JobDetailPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label LabelName;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label LabelDescription;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(JobDetailPage));
+            LabelName = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "LabelName");
+            LabelDescription = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "LabelDescription");
         }
     }
 }
